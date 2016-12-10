89ce77c2666d3abc58c26e6e5d5b3b7eb273338c
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-12-10 18:45:26 +0100

    Update install_packet_kali.sh

--- install_packet_kali.sh
+++ install_packet_kali.sh
@@ -2,6 +2,8 @@ echo "" >> /etc/apt/sources.list
 echo "#Kali linux repositories" >> /etc/apt/sources.list
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 
+gpg --keyserver pgpkeys.mit.edu --recv-key ED444FF07D8D0BF6<br /> gpg -a --export ED444FF07D8D0BF6| sudo apt-key add -
+
 apt-get update
 apt-get install kali-archive-keyring -y
 apt-get update
